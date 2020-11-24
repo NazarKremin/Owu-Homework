67676639b3bc9d58d685e22d9948fd5dc7811d6e
Nazar <72767552+NazarKremin@users.noreply.github.com>
2020-11-24 17:59:36 +0200

    Add files via upload

--- L10/JS/L10.js
+++ L10/JS/L10.js
@@ -70,236 +70,26 @@
 // }
 // printer()
 
-// let arrText =[]
-// arrText.push(prompt('Введіть речення (Максимум 11 знаків)'))
-// let div =document.getElementById('div')
-//
-// function wordOne(text){
-//     return new Promise(((resolve, reject) => {
-//         let random=Math.floor((Math.random() * (1 - 0.3) + 0.3)* 10) / 10;
-//         console.log(random)
-//         setTimeout(() => {
-//
-//                 let oneText=text[0].substr(0,1)
-//
-//                 if (oneText!=='' ){
-//                     resolve(div.innerText=`${oneText}`);
-//                 }else {
-//                     reject('Текст введено');
-//                 }
-//             },
-//             random*=1000)
-//
-//
-//     }))
+// function random(ms){
+//     return Math.round(Math.random() * ms);
 // }
-// function wordTwo(text,arrtext){
-//     return new Promise(((resolve, reject) => {
-//         let random=Math.floor((Math.random() * (1 - 0.3) + 0.3)* 10) / 10;
-//         console.log(random)
-//         setTimeout(() => {
-//             let twoText=arrtext[0].substr(1,1)
+// let div = document.getElementById('div')
 //
-//             if (twoText!=='' ){
-//                 resolve(div.innerText=`${text}${twoText}`);
-//             }else {
-//                 reject('Текст введено');
-//             }
-//         }, random*=1000);
-//
-//     }))
-// }
-// function wordThree(text,arrtext){
-//     return new Promise(((resolve, reject) => {
-//         let random=Math.floor((Math.random() * (1 - 0.3) + 0.3)* 10) / 10;
-//         console.log(random)
-//         setTimeout(() => {
-//             let threeText=arrtext[0].substr(2,1)
-//
-//             if (threeText!=='' ){
-//                 resolve(div.innerText=`${text}${threeText}`);
-//             }else {
-//                 reject('Текст введено');
-//             }
-//         }, random*=1000);
-//
-//     }))
-// }
-// function wordFour(text,arrtext){
-//     return new Promise(((resolve, reject) => {
-//         let random=Math.floor((Math.random() * (1 - 0.3) + 0.3)* 10) / 10;
-//         console.log(random)
-//         setTimeout(() => {
-//             let fourText=arrtext[0].substr(3,1)
-//
-//             if (fourText!=='' ){
-//                 resolve(div.innerText=`${text}${fourText}`);
-//             }else {
-//                 reject('Текст введено');
-//             }
-//         }, random*=1000);
-//
-//     }))
-// }
-// function wordFive(text,arrtext){
-//     return new Promise(((resolve, reject) => {
-//         let random=Math.floor((Math.random() * (1 - 0.3) + 0.3)* 10) / 10;
-//         console.log(random)
-//         setTimeout(() => {
-//             let fiveText=arrtext[0].substr(4,1)
-//
-//             if (fiveText!=='' ){
-//                 resolve(div.innerText=`${text}${fiveText}`);
-//             }else {
-//                 reject('Текст введено');
-//             }
-//         }, random*=1000);
-//
-//     }))
-// }
-// function wordSix(text,arrtext){
-//     return new Promise(((resolve, reject) => {
-//         let random=Math.floor((Math.random() * (1 - 0.3) + 0.3)* 10) / 10;
-//         console.log(random)
-//         setTimeout(() => {
-//             let sixText=arrtext[0].substr(5,1)
-//
-//             if (sixText!=='' ){
-//                 resolve(div.innerText=`${text}${sixText}`);
-//             }else {
-//                 reject('Текст введено');
-//             }
-//         }, random*=1000);
-//
-//     }))
-// }
-// function wordSeven(text,arrtext){
-//     return new Promise(((resolve, reject) => {
-//         let random=Math.floor((Math.random() * (1 - 0.3) + 0.3)* 10) / 10;
-//         console.log(random)
-//         setTimeout(() => {
-//             let sevenText=arrtext[0].substr(6,1)
-//
-//             if (sevenText!=='' ){
-//                 resolve(div.innerText=`${text}${sevenText}`);
-//             }else {
-//                 reject('Текст введено');
-//             }
-//         }, random*=1000);
-//
-//     }))
-// }
-// function wordEight(text,arrtext){
-//     return new Promise(((resolve, reject) => {
-//         let random=Math.floor((Math.random() * (1 - 0.3) + 0.3)* 10) / 10;
-//         console.log(random)
-//         setTimeout(() => {
-//             let eightText=arrtext[0].substr(7,1)
-//
-//             if (eightText!=='' ){
-//                 resolve(div.innerText=`${text}${eightText}`);
-//             }else {
-//                 reject('Текст введено');
-//             }
-//         }, random*=1000);
-//
-//     }))
-// }
-// function wordNine(text,arrtext){
-//     return new Promise(((resolve, reject) => {
-//         let random=Math.floor((Math.random() * (1 - 0.3) + 0.3)* 10) / 10;
-//         console.log(random)
-//         setTimeout(() => {
-//             let nineText=arrtext[0].substr(8,1)
-//
-//             if (nineText!=='' ){
-//                 resolve(div.innerText=`${text}${nineText}`);
-//             }else {
-//                 reject('Текст введено');
-//             }
-//         }, random*=1000);
-//
-//     }))
-// }
-// function wordTen(text,arrtext){
-//     return new Promise(((resolve, reject) => {
-//         let random=Math.floor((Math.random() * (1 - 0.3) + 0.3)* 10) / 10;
-//         console.log(random)
-//         setTimeout(() => {
-//             let tenText=arrtext[0].substr(9,1)
-//
-//             if (tenText!=='' ){
-//                 resolve(div.innerText=`${text}${tenText}`);
-//             }else {
-//                 reject('Текст введено');
-//             }
-//         }, random*=1000);
-//
-//     }))
+// function input(text){
+//     return new  Promise(resolve => {
+//             setTimeout(()=>{
+//                 div.innerHTML += text;
+//              resolve()
+//          },random(1000))
+//     })
 // }
 //
-// function wordEleven(text,arrtext){
-//     return new Promise(((resolve, reject) => {
-//         let random=Math.floor((Math.random() * (1 - 0.3) + 0.3)* 10) / 10;
-//         console.log(random)
-//         setTimeout(() => {
-//             let elevenText=arrtext[0].substr(10,1)
-//
-//             if (elevenText!=='' ){
-//                 resolve(div.innerText=`${text}${elevenText}`);
-//             }else {
-//                 reject('Текст введено');
-//             }
-//         }, random*=1000);
-//
-//     }))
-// }
-// function wordTwelve(text,arrtext){
-//     return new Promise(((resolve, reject) => {
-//         let random=Math.floor((Math.random() * (1 - 0.3) + 0.3)* 10) / 10;
-//         console.log(random)
-//         setTimeout(() => {
-//             let twelveText=arrtext[0].substr(11,1)
-//
-//             if (twelveText!=='' ){
-//                 resolve(div.innerText=`${text}${twelveText}`);
-//             }else {
-//                 reject('Текст введено');
-//             }
-//         }, random*=1000);
-//
-//     }))
-// }
-// async function Text() {
-//     try {
-//         const one = await wordOne(arrText)
-//         console.log(one)
-//         const two = await wordTwo(one, arrText)
-//         console.log(two)
-//         const three = await wordThree(two, arrText)
-//         console.log(three)
-//         const four = await wordFour(three, arrText)
-//         console.log(four)
-//         const five = await wordFive(four, arrText)
-//         console.log(five)
-//         const six = await wordSix(five, arrText)
-//         console.log(six)
-//         const seven =await wordSeven(six,arrText)
-//         console.log(seven)
-//         const eight=await wordEight(seven,arrText)
-//         console.log(eight)
-//         const nine=await  wordNine(eight,arrText)
-//         console.log(nine)
-//         const ten=await wordTen(nine,arrText)
-//         console.log(ten)
-//         const eleven=await wordEleven(ten,arrText)
-//         console.log(eleven)
-//         const twelve=await wordTwelve(eleven,arrText)
-//         console.log(twelve)
-//
-//     }catch (e){
-//         console.log(e)
-//         alert(e)
+// async function output(){
+//     let prompt1 = prompt("")
+//     let texts = prompt1.split('')
+//     for (const string of texts){
+//        await input(string);
 //     }
+//
 // }
-// Text()
+// output()
